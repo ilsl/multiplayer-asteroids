14d483324667bb67edd9a9527c804b86a2aa72e2
isobeljones <isobel.jones@royalmail.com>
2019-03-13 17:02:47 +0000

    Adding comment that run_server should not be used.

--- run_server.py
+++ run_server.py
@@ -1,9 +1,11 @@
+# Don't run! Instead run the server.py
+
 import server
 
 if __name__ == "__main__":
-    running_server = server.Server(port=5555, server='')
     # Set the first Client to connect with an id of 0
     currentId = "0"
-    # Get the object poitions of a server
+    running_server = server.Server(port=5555, server='', currentId=currentId)
+    # Get the object positions of a server
     pos = running_server.build_object_pos()
     running_server.create_socket()
